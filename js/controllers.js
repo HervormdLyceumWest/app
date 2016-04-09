ff6d60947d5b2d0fc35737af7d6bdcf0b899f5db
HervormdLyceumWest <HervormdLyceumWest@users.noreply.github.com>
2016-04-09 20:08:52 +0200

    Update controllers.js
    google maps plattegrond en watch poition merge
    1st try

--- js/controllers.js
+++ js/controllers.js
@@ -32,103 +32,165 @@ angular.module('app.controllers', [])
   //////////////////////////////////////////////////////
 
 
-  var options = {timeout: 10000, enableHighAccuracy: true};
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+        // Initialize the map and the custom overlay.
+  function initMap() {
+    var index=0;
     var mapOptions = {
-    center: {lat: 52.362398, lng: 4.825519},
-    zoom: 19,
-    scrollwheel: false,
-    navigationControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    draggable: true,
-    disableDefaultUI: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-      mapTypeIds: [
-        google.maps.MapTypeId.ROADMAP,
-        google.maps.MapTypeId.TERRAIN
+       center: {lat: 52.362398, lng: 4.825519},
+      zoom: 12, //19
+      scrollwheel: false,
+      navigationControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      draggable: false,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: [
+          google.maps.MapTypeId.ROADMAP,
+          google.maps.MapTypeId.TERRAIN
         ]
       }
     };
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }); // Close Cordova Geolocation
+  map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+      
+      //////////////////////////////////////
+      ////////////geolocation///////////////
+      /////////////////////////////////////
+
+
+    // Try HTML5 geolocation.
+    //http://jsfiddle.net/thinkingstiff/rsp22/
+    marker = null;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(function(position,options) {
+
+          time=setInterval(function(){
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            if(marker == null) {
+              marker=new google.maps.Marker({
+                position:pos,
+                map:map,
+                optimized:false,
+                icon:'media/Blue_Ball.png'
+             })
+            }
+           marker.setPosition(pos);
+          },1000);
+
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+          
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+    
+
+
+
+      //////////////////////////////////////
+      ////////////overlay//////////////////
+      /////////////////////////////////////
+
+        var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(52.362200, 4.82440),
+            new google.maps.LatLng(52.362605, 4.825910));
+
+        // The photograph is courtesy of the U.S. Geological Survey.
+        var srcImage = 'media/2-A.png';
+
+        // The custom USGSOverlay object contains the USGS image,
+        // the bounds of the image, and a reference to the map.
+        overlay = new USGSOverlay(bounds, srcImage, map);
+      }
+
+      /** @constructor */
+      function USGSOverlay(bounds, image, map) {
+
+        // Initialize all properties.
+        this.bounds_ = bounds;
+        this.image_ = image;
+        this.map_ = map;
+
+        // Define a property to hold the image's div. We'll
+        // actually create this div upon receipt of the onAdd()
+        // method so we'll leave it null for now.
+        this.div_ = null;
+
+        // Explicitly call setMap on this overlay.
+        this.setMap(map);
+      }
+
   
+      USGSOverlay.prototype.onAdd = function() {
 
-  //////////////////////////////////////
-  ////////////geolocation///////////////
-  //////////////////////////////////////
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
 
-  // Try HTML5 geolocation.
-  //http://jsfiddle.net/thinkingstiff/rsp22/
-  marker = null;
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-  time=setInterval(function(){
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position,options) {
+        // Create the img element and attach it to the div.
+        var img = document.createElement('img');
+        img.src = this.image_;
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.position = 'absolute';
+        div.appendChild(img);
 
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+        this.div_ = div;
 
-        if(marker == null) {
-          marker=new google.maps.Marker({
-            position:pos,
-            map:map,
-            optimized:false,
-            icon:'img/Blue_Ball.png'
-         })
-        }
-      marker.setPosition(pos);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-        
+        // Add the element to the "overlayLayer" pane.
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);
+      };
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                            'Error: The Geolocation service failed.' :
-                            'Error: Your browser doesn\'t support geolocation.');
-    }
-  },3000);
+      USGSOverlay.prototype.draw = function() {
 
+        // We use the south-west and north-east
+        // coordinates of the overlay to peg it to the correct position and size.
+        // To do this, we need to retrieve the projection from the overlay.
+        var overlayProjection = this.getProjection();
 
+        // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
 
+        // Resize the image's div to fit the indicated dimensions.
+        var div = this.div_;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';
+        div.style.width = (ne.x - sw.x) + 'px';
+        div.style.height = (sw.y - ne.y) + 'px';
+      };
 
+      // The onRemove() method will be called automatically from the API if
+      // we ever set the overlay's map property to 'null'.
+      USGSOverlay.prototype.onRemove = function() {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+      };
 
-  ////////////////////////////////////////////////////
-  ////////////Testing GEO support msgs///////////////
-  //////////////////////////////////////////////////
-/*
-  if(!!navigator.geolocation) {
-
-    // HTML5 geo location supported
-    console.log("HTML5 geo location supported");
-    var alertPopup = $ionicPopup.alert({
-      title: 'HTML5 geo support',
-      template: 'Yes'
-    });
-  } else {
-    // HTML5 geo location supported
-    console.log("HTML5 geo location NOT supported");
-    var alertPopup = $ionicPopup.alert({
-      title: 'HTML5 geo support',
-      template: 'No'
-    });
-  }
- */
-
+      google.maps.event.addDomListener(window, 'load', initMap);
 }) // Close controller
    
 .controller('agendaCtrl', function($scope) {
