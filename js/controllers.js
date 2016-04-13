5455930383d4439021140b6278d0fd2532f0c4ba
Frank de Leeuw <frank.de.leeuw@hva.nl>
2016-04-13 14:26:53 +0200

    asdfasdfd

--- js/controllers.js
+++ js/controllers.js
@@ -113,7 +113,7 @@ angular.module('app.controllers', [])
       new google.maps.LatLng(52.362605, 4.825910));
 
     // The photograph is courtesy of the U.S. Geological Survey.
-    var srcImage = 'img/2e_verdieping.png';
+    var srcImage = 'img/2-A.png';
 
     // The custom USGSOverlay object contains the USGS image,
     // the bounds of the image, and a reference to the map.
@@ -162,8 +162,14 @@ angular.module('app.controllers', [])
 
   USGSOverlay.prototype.draw = function() {
 
+    // We use the south-west and north-east
+    // coordinates of the overlay to peg it to the correct position and size.
+    // To do this, we need to retrieve the projection from the overlay.
     var overlayProjection = this.getProjection();
 
+    // Retrieve the south-west and north-east coordinates of this overlay
+    // in LatLngs and convert them to pixel coordinates.
+    // We'll use these coordinates to resize the div.
     var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
     var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
 
@@ -175,7 +181,8 @@ angular.module('app.controllers', [])
     div.style.height = (sw.y - ne.y) + 'px';
   };
 
-
+  // The onRemove() method will be called automatically from the API if
+  // we ever set the overlay's map property to 'null'.
   USGSOverlay.prototype.onRemove = function() {
     this.div_.parentNode.removeChild(this.div_);
     this.div_ = null;
@@ -186,74 +193,7 @@ angular.module('app.controllers', [])
 
 
 
- var bounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(52.362200, 4.82440),
-      new google.maps.LatLng(52.362605, 4.825910));
 
-    // The photograph is courtesy of the U.S. Geological Survey.
-    var srcImage = 'img/2e_verdieping.png';
-
-    overlay = new USGSOverlay(bounds, srcImage, map);
-    }
-
-  /** @constructor */
-  function USGSOverlay(bounds, image, map) {
-
-    // Initialize all properties.
-    this.bounds_ = bounds;
-    this.image_ = image;
-    this.map_ = map;
-
-    this.div_ = null;
-
-    // Explicitly call setMap on this overlay.
-    this.setMap(map);
-  }
-
-
-  USGSOverlay.prototype.onAdd = function() {
-
-    var div = document.createElement('div');
-    div.style.borderStyle = 'none';
-    div.style.borderWidth = '0px';
-    div.style.position = 'absolute';
-
-    // Create the img element and attach it to the div.
-    var img = document.createElement('img');
-    img.src = this.image_;
-    img.style.width = '100%';
-    img.style.height = '100%';
-    img.style.position = 'absolute';
-    div.appendChild(img);
-
-    this.div_ = div;
-
-    // Add the element to the "overlayLayer" pane.
-    var panes = this.getPanes();
-    panes.overlayLayer.appendChild(div);
-  };
-
-  USGSOverlay.prototype.draw = function() {
-
-    var overlayProjection = this.getProjection();
-
-    var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-    var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-    // Resize the image's div to fit the indicated dimensions.
-    var div = this.div_;
-    div.style.left = sw.x + 'px';
-    div.style.top = ne.y + 'px';
-    div.style.width = (ne.x - sw.x) + 'px';
-    div.style.height = (sw.y - ne.y) + 'px';
-  };
-
-  USGSOverlay.prototype.onRemove = function() {
-    this.div_.parentNode.removeChild(this.div_);
-    this.div_ = null;
-  };
-
-  google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
