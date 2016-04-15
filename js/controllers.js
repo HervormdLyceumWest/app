465c23e6eb2f632826bb73982ceedef1f9264a84
Frank de Leeuw <frank.de.leeuw@hva.nl>
2016-04-15 15:23:42 +0200

    button try 300678

--- js/controllers.js
+++ js/controllers.js
@@ -5,30 +5,7 @@ angular.module('app.controllers', [])
   // Set HLW logo
   $scope.pageTitle = "<img src=\"img/logo.png\" width=\"110px\" height=\"36px\">";
 
-  /////////////////////////////////////////////
-  ////////////Floorplan Buttons///////////////
-  ///////////////////////////////////////////
- 
- ////////button 1////////////
- $scope.showFirstFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showFirstFloor()',
-       template: 'Function activated.'
-    });
-  }
 
-  $scope.showSecondFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showSecondFloor()',
-       template: 'Function activated.'
-    });
-  }
-  $scope.showThirdFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showThirdFloor()',
-       template: 'Function activated.'
-    });
-  }
 
 
   var overlay;
@@ -135,7 +112,168 @@ angular.module('app.controllers', [])
     this.setMap(map);
   }
 
+
+  USGSOverlay.prototype.onAdd = function() {
+
+    var div = document.createElement('div');
+    div.style.borderStyle = 'none';
+    div.style.borderWidth = '0px';
+    div.style.position = 'absolute';
+
+    // Create the img element and attach it to the div.
+    var img = document.createElement('img');
+    img.src = this.image_;
+    img.style.width = '100%';
+    img.style.height = '100%';
+    img.style.position = 'absolute';
+    div.appendChild(img);
+
+    this.div_ = div;
+
+    // Add the element to the "overlayLayer" pane.
+    var panes = this.getPanes();
+    panes.overlayLayer.appendChild(div);
+  };
+
+  USGSOverlay.prototype.draw = function() {
+
+    var overlayProjection = this.getProjection();
+
+    var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+    var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+    // Resize the image's div to fit the indicated dimensions.
+    var div = this.div_;
+    div.style.left = sw.x + 'px';
+    div.style.top = ne.y + 'px';
+    div.style.width = (ne.x - sw.x) + 'px';
+    div.style.height = (sw.y - ne.y) + 'px';
+  };
+
+  USGSOverlay.prototype.onRemove = function() {
+    this.div_.parentNode.removeChild(this.div_);
+    this.div_ = null;
+  };
+
   google.maps.event.addDomListener(window, 'load', initMap);
+
+
+
+
+
+  /////////////////////////////////////////////
+  ////////////Floorplan Buttons///////////////
+  ///////////////////////////////////////////
+ 
+ ////////button 1////////////
+ $scope.showFirstFloor = function() {
+    var alertPopup = $ionicPopup.alert({
+       title: 'showFirstFloor()',
+       template: 'Function activated.'
+    });
+      var overlay;
+      USGSOverlay.prototype = new google.maps.OverlayView();
+
+      // Initialize the map and the custom overlay.
+  function initMap() {
+    var index=0;
+    var mapOptions = {
+       center: {lat: 52.362398, lng: 4.825519},
+      zoom: 19, //19
+      scrollwheel: false,
+      navigationControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      draggable: false,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: [
+          google.maps.MapTypeId.ROADMAP,
+          google.maps.MapTypeId.TERRAIN
+        ]
+      }
+    };
+  map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+      
+      //////////////////////////////////////
+      ////////////geolocation///////////////
+      /////////////////////////////////////
+
+
+    // Try HTML5 geolocation.
+    //http://jsfiddle.net/thinkingstiff/rsp22/
+    marker = null;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(function(position,options) {
+
+        time=setInterval(function(){
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          if(marker == null) {
+            marker=new google.maps.Marker({
+              position:pos,
+              map:map,
+              optimized:false,
+              icon:'img/Blue_Ball.png'
+           })
+          }
+          marker.setPosition(pos);
+        },1000);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+          
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+    
+
+
+
+      //////////////////////////////////////
+      ////////////overlay//////////////////
+      /////////////////////////////////////
+  
+    var bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(52.362200, 4.82440),
+      new google.maps.LatLng(52.362605, 4.825910));
+
+    // The photograph 
+    var srcImage = 'img/1e_verdieping.png';
+
+    overlay = new USGSOverlay(bounds, srcImage, map);
+  }
+
+  /** @constructor */
+  function USGSOverlay(bounds, image, map) {
+
+    // Initialize all properties.
+    this.bounds_ = bounds;
+    this.image_ = image;
+    this.map_ = map;
+
+    this.div_ = null;
+
+    // Explicitly call setMap on this overlay.
+    this.setMap(map);
+  }
 
 
   USGSOverlay.prototype.onAdd = function() {
@@ -180,13 +318,21 @@ angular.module('app.controllers', [])
     this.div_ = null;
   };
 
-  
+  google.maps.event.addDomListener(window, 'load', initMap);
+  }
 
-
-
-
-
-
+  $scope.showSecondFloor = function() {
+    var alertPopup = $ionicPopup.alert({
+       title: 'showSecondFloor()',
+       template: 'Function activated.'
+    });
+  }
+  $scope.showThirdFloor = function() {
+    var alertPopup = $ionicPopup.alert({
+       title: 'showThirdFloor()',
+       template: 'Function activated.'
+    });
+  }
 
 
 
