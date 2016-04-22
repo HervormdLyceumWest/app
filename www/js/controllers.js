20f40f6f6a49ef7222e8dc4c4917bd08b70800b4
Niels <Niels@MacBook-Pro-van-Niels.local>
2016-04-23 00:21:18 +0200

    Update background overlay
    - removed watch geo location (will return in future commit).

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,23 +9,18 @@ angular.module('app.controllers', [])
   ////////////Floorplan Buttons///////////////
   ///////////////////////////////////////////
   $scope.showFirstFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showFirstFloor()',
-       template: 'Function activated.'
-    });
+    showFirstFloor();
   }
   $scope.showSecondFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showSecondFloor()',
-       template: 'Function activated.'
-    });
+    showSecondFloor();
   }
   $scope.showThirdFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showThirdFloor()',
-       template: 'Function activated.'
-    });
+    showThirdFloor();
   }
+
+
+
+
 
   ////////////////////////////////////////////////////////
   ////////////Initialise GoogleMaps Canvas///////////////
@@ -37,7 +32,10 @@ angular.module('app.controllers', [])
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
  
     var mapOptions = {
+    // HLW LOCATION
     center: {lat: 52.362398, lng: 4.825519},
+    // GOOGLE TEST LOCATION
+    //center: {lat: 62.323907, lng: -150.109291},
     zoom: 19,
     scrollwheel: false,
     navigationControl: false,
@@ -55,47 +53,151 @@ angular.module('app.controllers', [])
     };
 
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }); // Close Cordova Geolocation
-  
 
-  //////////////////////////////////////
-  ////////////geolocation///////////////
-  //////////////////////////////////////
 
-   // Try HTML5 geolocation.
-    //http://jsfiddle.net/thinkingstiff/rsp22/
-    marker = null;
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
 
-   if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(function(position,options) {
 
-          time=setInterval(function(){
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+      function showFirstFloor(){
 
-            if(marker == null) {
-              marker=new google.maps.Marker({
-                position:pos,
-                map:map,
-                optimized:false,
-                icon:'media/Blue_Ball.png'
-             })
-            }
-           marker.setPosition(pos);
-          },1000);
+        var overlay;
+        USGSOverlay.prototype = new google.maps.OverlayView();
 
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(52.362200, 4.82440),
+            new google.maps.LatLng(52.362605, 4.825910));
+
+        // The photograph is courtesy of the U.S. Geological Survey.
+        var srcImage = 'img/2-A.png';
+
+        // The custom USGSOverlay object contains the USGS image,
+        // the bounds of the image, and a reference to the map.
+        overlay = new USGSOverlay(bounds, srcImage, map);
+
       }
+
+      function showSecondFloor(){
+        
+        var overlay;
+        USGSOverlay.prototype = new google.maps.OverlayView();
+
+        var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(52.362200, 4.82440),
+            new google.maps.LatLng(52.362605, 4.825910));
+
+        // The photograph is courtesy of the U.S. Geological Survey.
+        var srcImage = 'img/Blue-Ball.png';
+
+        // The custom USGSOverlay object contains the USGS image,
+        // the bounds of the image, and a reference to the map.
+        overlay = new USGSOverlay(bounds, srcImage, map);
+
+      }
+
+      function showThirdFloor(){
+        
+        var overlay;
+        USGSOverlay.prototype = new google.maps.OverlayView();
+
+        var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(52.362200, 4.82440),
+            new google.maps.LatLng(52.362605, 4.825910));
+
+        // The photograph is courtesy of the U.S. Geological Survey.
+        var srcImage = 'img/logo.png';
+
+        // The custom USGSOverlay object contains the USGS image,
+        // the bounds of the image, and a reference to the map.
+        overlay = new USGSOverlay(bounds, srcImage, map);
+
+      }
+
+      new showFirstFloor();
+
+
+
+
+
+
+
+// START PART 2: IMAGE OVERLAY
+ /** @constructor */
+      function USGSOverlay(bounds, image, map) {
+
+        // Initialize all properties.
+        this.bounds_ = bounds;
+        this.image_ = image;
+        this.map_ = map;
+
+        // Define a property to hold the image's div. We'll
+        // actually create this div upon receipt of the onAdd()
+        // method so we'll leave it null for now.
+        this.div_ = null;
+
+        // Explicitly call setMap on this overlay.
+        this.setMap(map);
+      }
+
+      /**
+       * onAdd is called when the map's panes are ready and the overlay has been
+       * added to the map.
+       */
+      USGSOverlay.prototype.onAdd = function() {
+
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
+
+        // Create the img element and attach it to the div.
+        var img = document.createElement('img');
+        img.src = this.image_;
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.position = 'absolute';
+        div.appendChild(img);
+
+        this.div_ = div;
+
+        // Add the element to the "overlayLayer" pane.
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);
+      };
+
+      USGSOverlay.prototype.draw = function() {
+
+        // We use the south-west and north-east
+        // coordinates of the overlay to peg it to the correct position and size.
+        // To do this, we need to retrieve the projection from the overlay.
+        var overlayProjection = this.getProjection();
+
+        // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+        // Resize the image's div to fit the indicated dimensions.
+        var div = this.div_;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';
+        div.style.width = (ne.x - sw.x) + 'px';
+        div.style.height = (sw.y - ne.y) + 'px';
+      };
+
+      // The onRemove() method will be called automatically from the API if
+      // we ever set the overlay's map property to 'null'.
+      USGSOverlay.prototype.onRemove = function() {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+      };
+// END PART2: IMAGE OVERLAY
+
+
+
+
+  }); // Close Cordova Geolocation
+
+
 
 
 }) // Close controller
