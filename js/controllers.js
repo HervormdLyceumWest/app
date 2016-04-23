57a2b04073c97556df8b21a4804c185a49919523
Niels <Niels@MacBook-Pro-van-Niels.local>
2016-04-23 02:24:57 +0200

    Update from master branch
    - Ionic from v1.2.4 to v1.3
    - fix floorpan
    - fix floorpan buttons
    
    TO DO:
    - show current user location with watchposition()
    - set right floorpan images on google maps canvas

--- js/controllers.js
+++ js/controllers.js
@@ -5,27 +5,6 @@ angular.module('app.controllers', [])
   // Set HLW logo
   $scope.pageTitle = "<img src=\"img/logo.png\" width=\"110px\" height=\"36px\">";
 
-  /////////////////////////////////////////////
-  ////////////Floorplan Buttons///////////////
-  ///////////////////////////////////////////
-  $scope.showFirstFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showFirstFloor()',
-       template: 'Function activated.'
-    });
-  }
-  $scope.showSecondFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showSecondFloor()',
-       template: 'Function activated.'
-    });
-  }
-  $scope.showThirdFloor = function() {
-    var alertPopup = $ionicPopup.alert({
-       title: 'showThirdFloor()',
-       template: 'Function activated.'
-    });
-  }
 
   ////////////////////////////////////////////////////////
   ////////////Initialise GoogleMaps Canvas///////////////
@@ -35,7 +14,6 @@ angular.module('app.controllers', [])
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
  
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
     var mapOptions = {
     center: {lat: 52.362398, lng: 4.825519},
     zoom: 19,
@@ -54,13 +32,149 @@ angular.module('app.controllers', [])
       }
     };
 
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }); // Close Cordova Geolocation
-  
 
-  //////////////////////////////////////
-  ////////////geolocation///////////////
-  //////////////////////////////////////
+  /////////////////////////////////////////////
+  ////////////Floorplan Buttons///////////////
+  ///////////////////////////////////////////
+
+    var overlay;
+    USGSOverlay.prototype = new google.maps.OverlayView();
+
+    function showFirstFloor(){
+      // Load google map
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      // Load floorplant overlay
+      var bounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(52.362200, 4.82440),
+          new google.maps.LatLng(52.362605, 4.825910));
+      var srcImage = 'img/2-A.png';
+      overlay = new USGSOverlay(bounds, srcImage, map);
+
+
+    }
+
+    function showSecondFloor(){
+      // Load google map
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      // Load floorplant overlay
+      var bounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(52.362200, 4.82440),
+          new google.maps.LatLng(52.362605, 4.825910));
+      var srcImage = 'img/smile.png';
+      overlay = new USGSOverlay(bounds, srcImage, map);
+
+
+    }
+
+    function showThirdFloor(){
+      // Load google map
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      // Load floorplant overlay
+      var bounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(52.362200, 4.82440),
+          new google.maps.LatLng(52.362605, 4.825910));
+      var srcImage = 'img/bier.jpg';
+      overlay = new USGSOverlay(bounds, srcImage, map);
+
+
+    }
+
+    new showFirstFloor();
+
+
+     $scope.showFirstFloor = function() {
+        showFirstFloor();
+      }
+      $scope.showSecondFloor = function() {
+        showSecondFloor();
+      }
+      $scope.showThirdFloor = function() {
+        showThirdFloor();
+      }
+
+
+  //////////////////////////////////////////////
+  ////////////Image overlay code///////////////
+  ////////////////////////////////////////////
+
+       /** @constructor */
+      function USGSOverlay(bounds, image, map) {
+
+        // Initialize all properties.
+        this.bounds_ = bounds;
+        this.image_ = image;
+        this.map_ = map;
+
+        // Define a property to hold the image's div. We'll
+        // actually create this div upon receipt of the onAdd()
+        // method so we'll leave it null for now.
+        this.div_ = null;
+
+        // Explicitly call setMap on this overlay.
+        this.setMap(map);
+      }
+
+      /**
+       * onAdd is called when the map's panes are ready and the overlay has been
+       * added to the map.
+       */
+      USGSOverlay.prototype.onAdd = function() {
+
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
+
+        // Create the img element and attach it to the div.
+        var img = document.createElement('img');
+        img.src = this.image_;
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.position = 'absolute';
+        div.appendChild(img);
+
+        this.div_ = div;
+
+        // Add the element to the "overlayLayer" pane.
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);
+      };
+
+      USGSOverlay.prototype.draw = function() {
+
+        // We use the south-west and north-east
+        // coordinates of the overlay to peg it to the correct position and size.
+        // To do this, we need to retrieve the projection from the overlay.
+        var overlayProjection = this.getProjection();
+
+        // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+        // Resize the image's div to fit the indicated dimensions.
+        var div = this.div_;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';
+        div.style.width = (ne.x - sw.x) + 'px';
+        div.style.height = (sw.y - ne.y) + 'px';
+      };
+
+      // The onRemove() method will be called automatically from the API if
+      // we ever set the overlay's map property to 'null'.
+      USGSOverlay.prototype.onRemove = function() {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+      };
+
+
+  //////////////////////////////////////////////////////
+  ////////////Show current user position///////////////
+  ////////////////////////////////////////////////////
 
    // Try HTML5 geolocation.
     //http://jsfiddle.net/thinkingstiff/rsp22/
@@ -96,8 +210,10 @@ angular.module('app.controllers', [])
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
       }
+    
 
 
+  }); // Close Cordova Geolocation
 }) // Close controller
    
 .controller('agendaCtrl', function($scope) {
