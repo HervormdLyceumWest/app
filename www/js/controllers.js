c30b0dc7da337f3ea7d642cfa41087c0ae166261
Niels <Niels@MacBook-Pro-van-Niels.local>
2016-05-13 23:07:45 +0200

    Update - aftermath
    - added start screen
    - disabled geotracking
    - fixed bugs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,16 @@
 angular.module('app.controllers', [])
 
+.controller('startCtrl', function($scope, $location) {
+
+  // Set HLW logo
+  $scope.pageTitle = "<a href=\"#/page1/start\"><img src=\"img/logo.png\" width=\"110px\" height=\"36px\"><a>";
+
+})
+
 .controller('plattegrondCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $timeout) {
 
   // Set HLW logo
-  $scope.pageTitle = "<img src=\"img/logo.png\" width=\"110px\" height=\"36px\">";
+  $scope.pageTitle = "<a href=\"#/page1/start\"><img src=\"img/logo.png\" width=\"110px\" height=\"36px\"><a>";
 
   ////////////////////////////////////////////////////////
   ////////////Initialise GoogleMaps Canvas///////////////
@@ -49,25 +56,29 @@ angular.module('app.controllers', [])
         var bounds = new google.maps.LatLngBounds(
             new google.maps.LatLng(52.361950, 4.82385),
             new google.maps.LatLng(52.362805, 4.827310));
-        var srcImage = 'img/beganegrond.jpg';
+        var srcImage = 'img/beganegrond.png';
       }
 
       if (floor==2){
         var bounds = new google.maps.LatLngBounds(
             new google.maps.LatLng(52.361950, 4.82385),
             new google.maps.LatLng(52.362805, 4.827310));
-        var srcImage = 'img/1e_verdieping.jpg';
+        var srcImage = 'img/1e_verdieping.png';
       }
 
       if (floor==3){
         var bounds = new google.maps.LatLngBounds(
             new google.maps.LatLng(52.361950, 4.82385),
             new google.maps.LatLng(52.362805, 4.827310));
-        var srcImage = 'img/2e_verdieping.jpg';
+        var srcImage = 'img/2e_verdieping.png';
       }
 
       // Create floorplan overlay (step 2/3)
       overlay = new USGSOverlay(bounds, srcImage, map);
+
+      // Step 3: geolocation (WORKING BUT DISABLED)
+      // Uncomment following code to enable geolocation
+      /*
 
       // Prepare the marker
       function setMarker(pos) {
@@ -88,8 +99,7 @@ angular.module('app.controllers', [])
         });
       }
 
-
-      // Create current location marker (step 3/3)
+      // Create current location marker
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setMarker, error);
 
@@ -103,7 +113,10 @@ angular.module('app.controllers', [])
       } else {
         alert("Your browser does not support the Geolocation API");
       }
-    }
+      */
+
+
+    } // Close buildMapWithOverlay
 
 
     // Initiliaze
@@ -203,13 +216,13 @@ angular.module('app.controllers', [])
 .controller('agendaCtrl', function($scope) {
 
   // Set HLW logo
-  $scope.pageTitle = "<img src=\"img/logo.png\" width=\"110px\" height=\"36px\">";
+  $scope.pageTitle = "<a href=\"#/page1/start\"><img src=\"img/logo.png\" width=\"110px\" height=\"36px\"><a>";
 
 })
 
 .controller('informatieCtrl', function($scope) {
 
   // Set HLW logo
-  $scope.pageTitle = "<img src=\"img/logo.png\" width=\"110px\" height=\"36px\">";
+  $scope.pageTitle = "<a href=\"#/page1/start\"><img src=\"img/logo.png\" width=\"110px\" height=\"36px\"><a>";
 
 })
